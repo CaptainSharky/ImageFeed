93608c9c8d4866c3c50538f3dbea64a2124e0ce4
CaptainSharky <chuikostepan@mail.ru>
2025-03-07 18:31:51 +0300

    feat(main): добавлена вёрстка ячейки таблицы

---
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Valeriy Chuiko on 03.03.2025.
+//
+
+import UIKit
+
+class ImagesListViewController: UIViewController {
+    @IBOutlet private var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        tableView.rowHeight = 200
+    }
+
+
+}
+
+extension ImagesListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+}
+
+extension ImagesListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentifier, for: indexPath)
+        
+        guard let imagesListCell = cell as? ImagesListCell else {
+            return UITableViewCell()
+        }
+        
+        configCell(for: imagesListCell)
+        return imagesListCell
+    }
+    
+    func configCell(for cell: ImagesListCell) { }
+}
