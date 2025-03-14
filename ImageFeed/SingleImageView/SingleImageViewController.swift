33d4bec6522eba036f4c6737be632fffa77ae8ce
CaptainSharky <chuikostepan@mail.ru>
2025-03-14 12:37:17 +0300

    feat(SingleImageViewController): добавлены значения зума для SingleImageView

--- ImageFeed/SingleImageView/SingleImageViewController.swift
+++ ImageFeed/SingleImageView/SingleImageViewController.swift
@@ -14,9 +14,20 @@ final class SingleImageViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         imageView.image = image
+        imageView.frame.size = image?.size ?? CGSize()
+        
+        // Значения для зума scrollView
+        scrollView.minimumZoomScale = 0.1
+        scrollView.maximumZoomScale = 1.25
     }
     
     @IBAction func didTapBackButton(_ sender: Any) {
         dismiss(animated: true, completion: nil)
+    }
+}
+
+extension SingleImageViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
     }
 }
